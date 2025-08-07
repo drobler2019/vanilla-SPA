6ac5b4daad9645e9168d48112e4c031067e58849
drobler2019 <drobler2019@gmail.com>
2025-08-06 23:33:17 -0500

    Descartado desestructuracion de objeto, agregado método para gestionar eventos, método que elimina el evento cuando se destruya el componente, paginación dinámica y estilo de página seleccionada

--- components/ProductsComponent.js
+++ components/ProductsComponent.js
@@ -4,9 +4,11 @@ export class ProductsComponent extends HTMLElement {
   constructor() {
     super();
     this.attachShadow({ mode: "open" });
+    this.page = 0;
   }
 
   async connectedCallback() {
+    this.response = await getProducts();
     this.shadowRoot.innerHTML = /*html */ `
     <style>
       .container-table {
@@ -60,14 +62,28 @@ export class ProductsComponent extends HTMLElement {
         }
 
         & > button:hover {
-          background-color: hwb(from var(--color-button) h w 20%);
+          background-color: rgb(from green r 60% b);
         }
+
+        & > button[disabled] {
+          background-color: hwb(from var(--color-button) h w b / 50% );
+        }
+      }
+
+      .button-selected {
+        background-color: rgb(from green r 60% b);
       }
        
     </style>
     `;
 
     this.shadowRoot.append(await this.createTamplate());
+    this.footer = this.shadowRoot.querySelector(".paginacion");
+    this.footer.addEventListener("click", this);
+  }
+
+  disconnectedCallback() {
+    this.footer.removeEventListener("click", this);
   }
 
   async createTamplate() {
@@ -86,15 +102,19 @@ export class ProductsComponent extends HTMLElement {
     const footer = document.createElement("footer");
     footer.className = "paginacion";
     const previous = document.createElement("button");
+    previous.setAttribute("disabled", "");
     previous.textContent = "Previous";
     const next = document.createElement("button");
     next.textContent = "Next";
     footer.append(previous, next);
-    const { lastElementChild: last } = footer;
     for (let p = 0; p < totalPages; p++) {
       const btn = document.createElement("button");
+      if (p === 0) {
+        btn.className = "button-selected";
+      }
+      btn.setAttribute("data-value", p.toString());
       btn.textContent = p + 1;
-      last.before(btn);
+      next.before(btn);
     }
     return footer;
   }
@@ -106,7 +126,80 @@ export class ProductsComponent extends HTMLElement {
       "<thead><tr><th>Id</th><th>Nombre</th><th>Descripción</th><th>Precio</th><th>Cantidad</th></tr></thead>"
     );
     const tbody = document.createElement("tbody");
-    this.response = await getProducts();
+    this.buildContent(tbody);
+    table.append(tbody);
+    return table;
+  }
+
+  async handleEvent(event) {
+    const previous =
+      this.shadowRoot.querySelector(".paginacion").firstElementChild;
+    const next = this.shadowRoot.querySelector(".paginacion").lastElementChild;
+    if (event.target.nodeName === "BUTTON") {
+      const btn = event.target;
+
+      const footer = this.shadowRoot.querySelector(".paginacion");
+      const removeSelected = footer.querySelector(".button-selected");
+      removeSelected.classList.remove("button-selected");
+      const btnSelected = footer.children[Number.parseInt(btn.textContent)];
+
+      if (btnSelected) {
+        btnSelected.className = "button-selected";
+      }
+
+      if (btn.textContent === "Next") {
+        const { nextElementSibling: selected } = removeSelected;
+        selected.className = "button-selected";
+        previous.removeAttribute("disabled");
+        if (this.page < this.response.totalPages) {
+          this.page++;
+        }
+        if (this.page === this.response.totalPages) {
+          next.setAttribute("disabled", "");
+        }
+      } else if (btn.textContent === "Previous") {
+        const { previousElementSibling: selected } = removeSelected;
+        selected.className = "button-selected";
+        if (this.page == 1) {
+          previous.setAttribute("disabled", "");
+        }
+        if (this.page > 0) {
+          this.page--;
+          if (this.page < this.response.totalPages - 1) {
+            next.removeAttribute("disabled");
+          }
+        }
+      } else {
+        this.page = Number.parseInt(btn.getAttribute("data-value"));
+      }
+
+      if (this.page > 0) {
+        previous.removeAttribute("disabled");
+        if (this.page === this.response.totalPages - 1) {
+          next.setAttribute("disabled", "");
+        }
+      }
+
+      if (this.page === 0) {
+        previous.setAttribute("disabled", "");
+        if (this.page < this.response.totalPages - 1) {
+          next.removeAttribute("disabled");
+        }
+      }
+
+      const table = this.shadowRoot.querySelector("table");
+      const { lastElementChild } = table;
+      const tbody = lastElementChild.cloneNode(true);
+      tbody.innerHTML = "";
+      this.response = await getProducts(this.page);
+      this.buildContent(tbody);
+      const fragment = document.createElement("template");
+      fragment.content.append(tbody);
+      table.replaceChild(fragment.content.cloneNode(true), lastElementChild);
+    }
+  }
+
+  buildContent(tbody) {
     this.response.content.forEach((product) => {
       const tr = document.createElement("tr");
       tr.insertAdjacentHTML(
@@ -115,8 +208,6 @@ export class ProductsComponent extends HTMLElement {
       );
       tbody.append(tr);
     });
-    table.append(tbody);
-    return table;
   }
 }
 
--- services/ProductsService.js
+++ services/ProductsService.js
@@ -5,11 +5,18 @@ const option = {
   headers: { "Content-Type": "application/json" },
 };
 
-export const getProducts = async () => {
-  const response = await fetch(url.href, option).then((response) =>
-    response.json()
-  );
-  const { content, numberOfElements, totalElements, totalPages } = response;
-  console.log({ content, numberOfElements, totalElements, totalPages });
-  return { content, numberOfElements, totalElements, totalPages };
+export const getProducts = async (page = 0) => {
+  url.searchParams.set("page", page);
+  return await fetch(url.href, option)
+    .then((response) => response.json())
+    .then((response) => {
+      const { content, numberOfElements, totalElements, totalPages } = response;
+      return { content, numberOfElements, totalElements, totalPages };
+    })
+    .catch(() => ({
+      content: [],
+      numberOfElements: 0,
+      totalElements: 0,
+      totalPages: 0,
+    }));
 };
